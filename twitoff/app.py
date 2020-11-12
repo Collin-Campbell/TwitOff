b5f0337433de8a309255a5248483a4e44078d7f0
collin-campbell <corocampbell@gmail.com>
2020-11-12 16:27:15 -0500

    Updated app.py again

--- twitoff/app.py
+++ twitoff/app.py
@@ -25,24 +25,6 @@ def create_app():
         # renders base.html template and passes down title and users
         return render_template('base.html', title="Home", users=User.query.all())
 
-    @app.route('/compare', methods=["POST"])  # http://127.0.0.1:5000/compare
-    def compare():
-        user0, user1 = sorted(
-            [request.values['user1'], request.values['user2']])
-
-        if user0 == user1:
-            message = "Cannot compare users to themselves!"
-
-        else:
-            prediction = predict_user(
-                user0, user1, request.values['tweet_text'])
-            message = '{} is more likely to be said by {} than {}'.format(
-                request.values["tweet_text"], user1 if prediction else user0,
-                user0 if prediction else user1
-            )
-
-        return render_template('prediction.html', title="Prediction", message=message)
-
     @app.route('/user', methods=["POST"])  # http://127.0.0.1:5000/user
     # http://127.0.0.1:5000/user/<name>
     @app.route('/user/<name>', methods=["GET"])
@@ -61,16 +43,34 @@ def create_app():
 
         return render_template("user.html", title=name, tweets=tweets, message=message)
 
-    @app.route('/update')  # http://127.0.0.1:5000/update
-    def update():
-        update_all_users()
-        return render_template('base.html', title="Home", users=User.query.all())
+    @app.route('/compare', methods=["POST"])  # http://127.0.0.1:5000/compare
+    def compare():
+        user0, user1 = sorted(
+            [request.values['user1'], request.values['user2']])
 
-    @app.route('/reset')  # http://127.0.0.1:5000/reset
+        if user0 == user1:
+            message = "Cannot compare users to themselves!"
+
+        else:
+            prediction = predict_user(
+                user0, user1, request.values['tweet_text'])
+            message = '{} is more likely to be said by {} than {}'.format(
+                request.values["tweet_text"], user1 if prediction else user0,
+                user0 if prediction else user1
+            )
+
+        return render_template('prediction.html', title="Prediction", message=message)
+
+    @app.route('/reset')
     def reset():
-        # we must create the database
         DB.drop_all()
         DB.create_all()
+        return render_template('base.html', title='Reset database!')
+
+    @app.route('/update')
+    def update():
+        update_all_users()
         return render_template('base.html', users=User.query.all(),
-                               title='All Tweets updated!')
+                               title='Cache cleared and all Tweets updated!')
+
     return app
